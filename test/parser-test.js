0dc14d43e7f23519f3aba5b139aacf806b4f2684
Lester Lyu <lester.lyu@ontario.ca>
2019-05-17 13:53:30 -0400

    Optimize DataValidations.

--- test/parser-test.js
+++ test/parser-test.js
@@ -1,79 +1,30 @@
 "use strict";
+const fs = require('fs');
 const XlsxPopulate = require('../lib/XlsxPopulate');
-const MAX_ROW = 1048576, MAX_COLUMN = 16384;
 
-let t = Date.now();
-let parser, depParser, wb, rt;
+const files = [];
+const extensions = ['.xlsx', '.xlsm', '.xlsb'];
 
-/**
- *
- * @param {Workbook} workbook
- */
-function something(workbook) {
-    wb = workbook;
-    console.log(`open workbook uses ${Date.now() - t}ms`);
-    t = Date.now();
-    // console.log(workbook.sheet('Fin_Summary').cell('I11').formula());
-    // console.log(workbook.sheet('Act_Summary').cell('H3176').formula());
-    // workbook.sheet('Act_Summary').cell('H11').setValue(123);
-    // console.log(workbook.sheet('Act_Summary').cell('I11').getValue());
-    // console.log(workbook.theme().themeColor(1));
-    // console.log(workbook.sheet(0).getCell(3, 3).getValue());
-    //
-    // console.log('...');
-    //
-    // const cell1 = workbook.sheet('Fin_CMHP1').cell('E10');
-    // const cell2 = workbook.sheet('Fin_CMHP1').cell('F10');
-    // console.log(cell1.setValue(123));
-    // console.log(cell2.value());
-    // console.log(cell2.setValue(12));
-    // console.log(cell1.setValue(1234));
-    // console.log(cell2.value()); // should be 12
-    //
-    // console.log('...');
-    // const a1 = workbook.sheet(0).cell('A1');
-    // const a2 = workbook.sheet(0).cell('A2');
-    // const a3 = workbook.sheet(0).cell('A3');
-    // a1.value(1);
-    // a2.value(2);
-    // a3.setFormula('A1+A2');
-    // console.log(a3.value());
-    // a2.value(3);
-    // console.log(a3.value());
-
-    // console.log('...');
-    // const a4 = workbook.sheet(0).cell('A4');
-    // const a5 = workbook.sheet(0).cell('A5');
-    // a4.formula('1233');
-    // a5.setFormula('A4');
-    // console.log(a5.value());
-
-
-
-    // const cell = workbook.sheet(0).cell('AD18');
-    // const cell2 = workbook.sheet(0).cell('A10');
-    // const cell3 = workbook.sheet(0).cell('L3');
-    // cell.setValue(2009);
-    // console.log(cell2.getValue())
-    // console.log(cell3.getValue())
-
-    // workbook.sheet('Main').hyperlink('')
-
-    console.log(`process formulas uses ${Date.now() - t}ms, with ? formulas, query data uses ??ms`);
-    t = Date.now();
-    // get data
-    // console.log(JSON.stringify(rt._data).length)
-
-    console.log(`process formulas uses ${Date.now() - t}ms`);
-}
-const file = ['./TF33674675.xlsx', './test.xlsm']
-setTimeout(() => {
-    t = Date.now();
-    XlsxPopulate.fromFileAsync(file[0])
-        .then(something)
-        .then(() => {
-            wb.toFileAsync('./out.xlsx');
+describe('Read test', () => {
+    const fileNames = fs.readdirSync('./');
+    fileNames.forEach(file => {
+        const i = file.lastIndexOf('.');
+        if (i < 0) return;
+        if (extensions.includes(file.slice(i)) && file.indexOf('out') === -1) {
+            files.push(file);
+        }
+    });
+    files.forEach(file => {
+        it(`should read ${file}`, done => {
+            XlsxPopulate.fromFileAsync(file)
+                .then(workbook => {
+                    done();
+                    return workbook;
+                });
+            // .then(workbook => {
+            //     workbook.toFileAsync(`${file.slice(0, file.lastIndexOf('.'))}.out${file.slice(file.lastIndexOf('.'))}`);
+            //     done();
+            // });
         });
-
-}, 0);
-
+    });
+});
